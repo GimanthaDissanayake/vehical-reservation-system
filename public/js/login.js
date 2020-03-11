d6681e41bd685a6b522b032e2fa72e6b1f0f7360
Hirantha <hiranthapeiris404@gmail.com>
2020-03-11 21:51:04 +0530

    inquiry page devs

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,7 @@
-$("#btnLogin").click(function () {
-    alert("Handler login clicked");
-});
+$(document).ready(function () {
+    $("#btnLogin").click(function () {
+        let login_url = "./login.html";
+        document.location.href = login_url;
+        // alert("Handler login clicked");
+    });
+})
